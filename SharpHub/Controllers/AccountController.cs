59d4b258833a8c80cd1153bf5eab0b6cdb5f213f
Lentokone <olli22012@student.hamk.fi>
2025-06-07 01:22:26 +0300

    Added controller

---
+++ SharpHub/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SharpHub.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
