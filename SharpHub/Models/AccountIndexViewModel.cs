134887454d53e9c8ffa44d0dad082ad425117a9e
Lentokone <olli22012@student.hamk.fi>
2025-06-19 01:48:27 +0300

    Continued on login stuff

---
+++ SharpHub/Models/AccountIndexViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SharpHub.Models
+{
+    public class AccountIndexViewModel
+    {
+        public UserLoginViewModel Login { get; set; } = new UserLoginViewModel();
+        public UserRegisterViewModel Register { get; set; } = new UserRegisterViewModel();
+
+    }
+}
