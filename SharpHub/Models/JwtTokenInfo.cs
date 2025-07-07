d08ae0596cb5aaec5c89cd20a4e9ba5df29462f3
Lentokone <olli22012@student.hamk.fi>
2025-07-08 00:26:11 +0300

    Made classes for logging and jwt token

---
+++ SharpHub/Models/JwtTokenInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace SharpHub.Models
+{
+    public class JwtTokenInfo
+    {
+        public required string Token { get; set; }
+        public DateTime ExpiresAt { get; set; }
+        public DateTime IssuedAt { get; set; }
+    }
+}
---
+++ SharpHub/Models/RepoTokenSession.cs
@@ -0,0 +1,14 @@
+﻿namespace SharpHub.Models
+{
+    public class RepoTokenSession : DB_SaveableObject
+    {
+        public string UserId { get; set; }
+        public string RepoId { get; set; }
+        public string RefreshToken { get; set; }
+        public bool IsActive { get; set; }
+        public JwtTokenInfo? JwtToken { get; set; }
+        public DateTime ExpirationDate { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastUsedAt { get; set; }
+    }
+}
