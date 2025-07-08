280e0491b43c3fd32d39737ba980763981314131
Lentokone <olli22012@student.hamk.fi>
2025-07-08 14:15:35 +0300

    Something

--- SharpHub/Models/RefreshToken.cs
+++ SharpHub/Models/RefreshToken.cs
@@ -2,10 +2,11 @@
 {
     public class RefreshToken : DB_SaveableObject
     {
-        public string Token { get; set; }
+        public required string Token { get; set; }
         public int UserId { get; set; }
         public DateTime Expiry { get; set; }
         public bool IsRevoked { get; set; }
         public DateTime CreatedAt { get; set; }
+        public JwtTokenInfo?  Jwt { get; set; }
     }
 }
