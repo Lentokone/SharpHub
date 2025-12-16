8cb513f7fbcc41c4665ecaa7be0fd0d307ee5f0e
Lentokone <olli22012@student.hamk.fi>
2025-12-17 01:02:20 +0200

    Removed old test model

--- SharpHub/Models/REPOMANAGERtestTEST.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SharpHub.Models
-{
-    public class REPOMANAGERtestTEST
-    {
-        public IEnumerable<Repository> Repositories { get; set; } = new List<Repository>();
-
-        public Repository NewRepository { get; set; } = new Repository("Testt", "ttest", "", "jep");
-    }
-}
