ee31261d97a11995eb6583d21b2ff038afae05a1
Lentokone <olli22012@student.hamk.fi>
2026-05-18 20:13:39 +0300

    Removed redundant things
    Maybe

--- SharpHub/Program.cs
+++ SharpHub/Program.cs
@@ -7,8 +7,6 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddRazorComponents();
 builder.Services.AddServerSideBlazor();
 
-builder.Services.AddSingleton<JwtTokenService>();
-
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
     .AddCookie(options =>
     {
